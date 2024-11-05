6f6619fdf12de713d3e7e4ee71f46075eb9fa333
Mayur Gohane <mayur100888@gmail.com>
2024-11-05 17:56:12 +0530

    Added

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'Generative AI Project',
+    name = 'Generative AI',
     version= '0.0.0',
-    author= 'Bappy Ahmed',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Mayur Gohane',
+    author_email= 'mayurgohane19@gmail.com',
     packages= find_packages(),
     install_requires = []
 
